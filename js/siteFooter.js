5afedb04928abba5bac689dab8c103a4ed1b6b3d
R Ajeti <r4ajeti@gmail.com>
2018-12-09 18:52:20 +0100

    UPDATED - New Header Created ADDED - Responsive header Github testing Definimi i pjeseve statike BUG FIXED - filmatCss.css updated INPROGRESS - Stub skeleton process

---
+++ js/siteFooter.js
@@ -0,0 +1,53 @@
+document.writeln(`<div id="siteFooter">
+		<div id="NewsLetter">
+			<div class="contentNews">
+				<h4>Stay In The Know!</h4><hr />
+				<input type="text" class="NewsletterT" value="" />
+				<input type="button" class="NewsletterB" value="GO">
+				<p>To unsubscribe please click here >></p>
+			</div>
+		</div>
+		<div class="NewsContent">
+
+			<div class="Element leftN">
+				<h4>Services</h4>
+				<p>Lorem ipsum dolor
+					Suspendisse in neque
+					Praesent et eros
+					Lorem ipsum dolor
+					Suspendisse in neque Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa eius nisi tenetur volupta
+					te! Amet architecto, culpa debitis delectus distinctio eveniet facere iusto laudantium minima officia quam quo,
+					reprehenderit, rerum voluptate? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet architecto in nemo nulla si
+					nt. Ad autem dignissimos esse ipsam minima obcaecati quo sequi veritatis, voluptates! Alias eveniet explicabo praesentium sint?</p>
+			</div>
+
+			<div class="Element rightN">
+				<h4>Title Text</h4>
+				<p>Lorem ipsum dolor
+					Suspendisse in neque
+					Praesent et eros
+					Lorem ipsum dolor
+					Suspendisse in neque Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa eius nisi tenetur volupta
+					te! Amet architecto, culpa debitis delectus distinctio eveniet facere iusto laudantium minima officia quam quo,
+					reprehenderit, rerum voluptate? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet architecto in nemo nulla si
+					nt. Ad autem dignissimos esse ipsam minima obcaecati quo sequi veritatis, voluptates! Alias eveniet explicabo praesentium sint?</p>
+			</div>
+
+			<div class="Element centerN">
+				<h4>Title Text</h4>
+				<p>Lorem ipsum dolor
+					Suspendisse in neque
+					Praesent et eros
+					Lorem ipsum dolor
+					Suspendisse in neque Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa eius nisi tenetur volupta
+					te! Amet architecto, culpa debitis delectus distinctio eveniet facere iusto laudantium minima officia quam quo,
+					reprehenderit, rerum voluptate? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet architecto in nemo nulla si
+					nt. Ad autem dignissimos esse ipsam minima obcaecati quo sequi veritatis, voluptates! Alias eveniet explicabo praesentium sint?</p>
+			</div>
+		</div>
+
+		<div id="CopyRight">
+			<p class="pL">Copyright &copy; 2013 Domain Name - All Rights Reserved</p>
+			<p class="pR">Template by R!n0</p>
+		</div>
+	</div>`);
