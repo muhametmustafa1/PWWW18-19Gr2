1c7e2624110a35320b9c08c0d3fb118ee39281fb
Muhamet Mustafa <muhametmustafa1@users.noreply.github.com>
2018-12-28 01:07:03 +0100

    Ridizajnimi i navbar ne filmat.html

--- js/filmatNavBar.js
+++ js/filmatNavBar.js
@@ -1,5 +1,12 @@
 function hapeMenune() {
 var linqet = document.getElementsByClassName('linqet');
+var a = document.getElementById("a");
+//var navbar = document.getElementById("navbar");
+
+if(a.backgroundColor !="black") {
+  a.style.backgroundColor = "black";
+}
+
 
 for (var i = 0; i < linqet.length; i++) {
     if(linqet[i].style.display === "none") {
@@ -7,6 +14,8 @@ for (var i = 0; i < linqet.length; i++) {
     }
     else {
       linqet[i].style.display ="none";
+      a.style.backgroundColor = "inherit";
+
     }
 } //end for
 
