61380eca8bc05dcc7cac61939dd3d4dc55624581
R Ajeti <r4ajeti@gmail.com>
2018-12-30 04:07:27 +0100

    INFO - Modified File 'introBackground.js'(minToRGBConvertor) ADDED - Offline Site Cache [-serviceWorker-] INFO - Blanked Commit UPDATED - liveStreaming.html carosel changed INFO - Writing Clear Code CREATED - liveStreaming.html ADDED - Thank you animation UPDATED - New Header Created ADDED - Responsive header Github testing Definimi i pjeseve statike BUG FIXED - filmatCss.css updated INPROGRESS - Stub skeleton process

--- js/introBackground.js
+++ js/introBackground.js
@@ -1,13 +1,46 @@
 var d = new Date();
 var oret = d.getHours();
+var minutat = koha.getMinutes();
 var intro = document.getElementById("intro");
 
 //Nese eshte mengjes deri ne ora 10 le te qendroje si eshte!!
-
-if(oret > 10 && oret <19) {
-  intro.style.backgroundColor= "#065F18"; //gjelber e mbyllur
+//blue -3
+var rgbColor = timeToRGBcolorConvertor(oret, minutat);
+if(rgbColor !== null){
+	intro.style.backgroundColor = `rgba(${rgbColor[0]},${rgbColor[1]},${rgbColor[2]},1)`; //kalter e mbyllur #FF0000
 }
-if (oret >=19 && oret < 24) {
-  intro.style.backgroundColor = "#06245F" //kalter e mbyllur
 
-}
+function timeToRGBcolorConvertor(hours, minutes){
+	if( (0<=hours && hours<24) && (0<=minutes && minutes<60) ){
+		var typesRGB = ['red','green','blue'];
+		var sizeRGB = typesRGB.length;
+		var valuesRGB = [0,0,0];
+		const hourToMin = 60;
+		const lowerBound = 6, upperBound = 22, measuringUnit = 8, maxPartialMinUnitVal = measuringUnit*hourToMin, maxPartialColUnitVal = 255*2;
+		var x = (hours) % measuringUnit;
+		var y = Math.floor( (hours) / measuringUnit );
+		console.log("testss: hours: "+hours+" x: "+x+" y: "+y);
+		var c;
+		
+		for (var i = 0; i <= sizeRGB - 1; i++) {
+			if(y == i%sizeRGB){typesRGB[i] = 1; c = i;}
+			else{}
+		}
+		
+		let time = x*hourToMin+minutes, halfPartialMinUnitVal = maxPartialMinUnitVal/2, halfPartialColUnitVal = maxPartialColUnitVal/2;
+		console.log("testss: c: "+c+" time: "+time+" y: "+y);
+		if(c != undefined){
+
+			if(time<halfPartialMinUnitVal){
+				valuesRGB[(c)%sizeRGB] = halfPartialColUnitVal - Math.floor( time*maxPartialColUnitVal/maxPartialMinUnitVal );
+				valuesRGB[(c+1)%sizeRGB] = halfPartialColUnitVal;
+			}
+			else{
+				valuesRGB[(c+1)%sizeRGB] = halfPartialColUnitVal;
+				valuesRGB[(c+2)%sizeRGB] = Math.floor( (time*maxPartialColUnitVal/maxPartialMinUnitVal) )-halfPartialMinUnitVal;
+			}
+			
+		}
+		return valuesRGB;
+	}else{ return null;}
+}	
