006ce8a154cfd576ee97e5260fe2cff4b7edfe91
Muhamet Mustafa <muhametmustafa1@users.noreply.github.com>
2019-01-01 23:44:48 +0100

    ndryshime ne liveStreaming

--- js/siteCarousel.js
+++ js/siteCarousel.js
@@ -28,9 +28,9 @@ document.writeln(`<link rel="stylesheet" type="text/css" href="css/css-library/d
 		<div class="sceneN">
 			<div class="scene">
 			  	<div class="carousel" >creed-2-posterImage.jpg
-				    <div class="carousel__cell"><img src="images/forText-poster.jpg" alt="nuk u gjet fotojaP2"></div>
-				    <div class="carousel__cell"><img src="images/terminator-6-posterImage.jpg" alt="nuk u gjet fotojaP2"></div>
-				    <div class="carousel__cell"><img src="images/creed-2-posterImage.jpg" alt="nuk u gjet fotojaP2"></div>
+				    <div class="carousel__cell"><a href="https://youtu.be/UajyeXD4CYw" target="_blank"><img src="https://static.mubasherpulse.com/8ca842d3fa88f473820647f07b31e4b2/1024.jpg" alt="nuk u gjet fotojaP2"></a></div>
+						<div class="carousel__cell"><a href="https://www.youtube.com/watch?v=5B9Z431VkOY" target="_blank"><img src="http://media.graytvinc.com/images/810*592/TALENT9.jpg" alt="nuk u gjet fotojaP2"></a></div>
+						<div class="carousel__cell"><a href="https://watch.nba.com/video/2018/06/08/0041700404-gsw-cle-game-4-recap" target="_blank"><img src="https://i.ytimg.com/vi/u8s4nFAz-yY/maxresdefault.jpg" alt="nuk u gjet fotojaP2"></a></div>
 			  	</div>
 			</div>
 		</div>
