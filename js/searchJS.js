a11bb846a7a3e246b7fb8f343dec7503a89f171b
R Ajeti <r4ajeti@gmail.com>
2019-01-07 01:30:29 +0100

    ADDED - Search by id function INFO - Blanked Commit(favicon added) INFO - Blanked Commit INFO - Modified File 'introBackground.js'(minToRGBConvertor) ADDED - Offline Site Cache [-serviceWorker-] INFO - Blanked Commit UPDATED - liveStreaming.html carosel changed INFO - Writing Clear Code CREATED - liveStreaming.html ADDED - Thank you animation UPDATED - New Header Created ADDED - Responsive header Github testing Definimi i pjeseve statike BUG FIXED - filmatCss.css updated INPROGRESS - Stub skeleton process

--- js/searchJS.js
+++ js/searchJS.js
@@ -1,16 +1,35 @@
-//Qitu fillon kodi i kerkimit
-         $(document).on('keypress',function(e) {
-          var kerko = ((document.getElementById("kerko").value).toLowerCase()).trim();
-          var isInDatabase = false;
-          isInDatabase = ($( (('kerko').toLowerCase()).trim() ).length == 0);
-          console.log("Kerko: " + kerko);
-          if((e.which == 13) && (isInDatabase)) {
+      //Qitu fillon kodi i kerkimit
+      //This function checks if @varable is null
+      function varExist(variable){
+              if(typeof(variable) !== "undefined") {
+                  //it exist
+                  var varExist=true;
+              }
+              else {
+                alert("Var not defind");
+                  //it doesn't
+                  var varExist=false;
+              }
+              return varExist;
+            }
+
+      $(document).ready(function(){
+        $(document).on('keypress',function(e) {
+          if(e.which == 13) {
+            var kerko = ((document.getElementById("kerko").value).toLowerCase()).trim();
+            var isInDatabase = false;
+            var isNull = document.getElementById(kerko);
+            isInDatabase = varExist(isNull);
+
+            alert('isInDatabase!' + isInDatabase);
               alert('You pressed enter!' + kerko);
-              $('html,body').animate({
-              scrollTop: $("#"+kerko).offset().top},
-              'slow');
+              if(isInDatabase){
+                $('html,body').animate({
+                scrollTop: $("#"+kerko).offset().top}, 'slow');
+              }  
           }
         });
+      });
       //Qitu mbaron kodi i kerkimit
 
       //Search button script
--- js/siteHead.js
+++ js/siteHead.js
@@ -1,4 +1,5 @@
 document.writeln(`
 	<link rel="icon" href="images/logo.png" sizes="32x32 48x48" type="image/png">
 	<link rel="stylesheet" href="css/myStyle.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	`);
