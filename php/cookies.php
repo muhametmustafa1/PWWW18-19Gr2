81b45345d690750e4f892ea695ed3f1cc21cc3fa
ArdianHoti18 <1ardianhoti@gmail.com>
2019-05-22 10:51:06 +0200

    cookie permiresim

--- php/cookies.php
+++ php/cookies.php
@@ -1,9 +1,11 @@
 <?php
-//perdorrimi i cookies ne phpp
-$vlera='ka nje vlere te qfardoshme';
-// E FSHIJME NESE IA NDRYSHOJM KOHEN PSH E BEJME -
-setcookie("CookiePerTestim", $vlera,time()+ 3600);
-echo $_COOKIE["CookiePerTestim"]; // ME HEREN E DYTE BEN,PRA REFRESH JA BEN
-echo "<br>";
-print_r($_COOKIE);
+$cookie_name = "user";
+$cookie_value = "CubiTv";
+setcookie($cookie_name, $cookie_value, time() + 5, "/"); // 86400 = 1 day
+if(!isset($_COOKIE[$cookie_name])) {
+     echo "Cookie named '" . $cookie_name . "' is not set!";
+} else {
+     echo "Cookie '" . $cookie_name . "' is set!<br>";
+     echo "Value is: " . $_COOKIE[$cookie_name];
+}
 ?>
